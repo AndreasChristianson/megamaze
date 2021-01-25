3c3538fc12d1c1823b133d9b8d3f67a137379e80
Andreas Christianson <sullage@pessimistic-it.com>
2021-01-24 20:42:16 -0600

    initial image list

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import title from '../images/title.webp'
+import mim from '../images/mim-full.webp'
+import vim from '../images/vim-full.webp'
 
 const PageContainer = styled.div``
+const images = [
+  title,
+  mim,
+  vim
+]
+
+const Image = styled.img`
+`
 
 export const Homepage = (props) => (
   <PageContainer>
-    <h1>Hello world</h1>
+    <h1>{'Mim vs Vim'}</h1>
     <p>{VERSION}</p>
+    {images.map((image, index) => 
+      <Image src={image} key={index}/>
+    )}
   </PageContainer>
 )
